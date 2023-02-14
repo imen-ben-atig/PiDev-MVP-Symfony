d9dc261aeede82bfd470c8ed710b638e09148324
ZarroukHassene <hassene.zarrouk@esprit.tn>
2023-02-14 02:11:30 +0100

    template integrated

---
+++ GameGalaxy_webapp/src/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/testfront', name: 'app_testfront')]
+    public function indexFRONT(): Response
+    {
+        return $this->render('FRONT/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+    #[Route('/testback', name: 'app_testback')]
+    public function indexBACK(): Response
+    {
+        return $this->render('BACK/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+}
