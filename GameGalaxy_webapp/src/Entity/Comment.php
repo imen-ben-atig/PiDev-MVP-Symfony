4f5b14da8f85cb01f53686922841b54b5840858e
Youssef El Amri <youssef.1707pralin@gmail.com>
2023-02-16 12:39:46 +0100

    crud

---
+++ GameGalaxy_webapp/src/Entity/Comment.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private $created_at;
+ 
+    #[ORM\Column(length: 255)]
+    private $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDateComment(): ?\DateTimeInterface
+    {
+        return $this->date_comment;
+    }
+
+    public function setDateComment(\DateTimeInterface $date_comment): self
+    {
+        $this->date_comment = $date_comment;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+ 
+    public function setCreatedAt(?\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+ 
+        return $this;
+    }
+ 
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+ 
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+ 
+        return $this;
+    }
+}
