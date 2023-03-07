4e01fea4bd2807e01a421fa0419b7e8b0a9aa4b1
fares kouki <fares.kouki@esprit.tn>
2023-03-07 14:19:31 +0100

    qr

---
+++ GameGalaxy_webapp/migrations/Version20230307130336.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230307130336 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE evenement (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, date DATETIME NOT NULL, description VARCHAR(255) DEFAULT NULL, duree INT DEFAULT NULL, capacite INT DEFAULT NULL, type VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE membre (id INT AUTO_INCREMENT NOT NULL, pseudo VARCHAR(255) NOT NULL, niveau_compte INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, id_membre_id INT NOT NULL, id_evenement_id INT NOT NULL, date DATETIME NOT NULL, INDEX IDX_42C84955EAAC4B6D (id_membre_id), INDEX IDX_42C849552C115A61 (id_evenement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955EAAC4B6D FOREIGN KEY (id_membre_id) REFERENCES membre (id)');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C849552C115A61 FOREIGN KEY (id_evenement_id) REFERENCES evenement (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955EAAC4B6D');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C849552C115A61');
+        $this->addSql('DROP TABLE evenement');
+        $this->addSql('DROP TABLE membre');
+        $this->addSql('DROP TABLE reservation');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
